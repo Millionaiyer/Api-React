9c7a6ea52e7fc71ecfa008402282f0a6cd7229f1
Millionaiyer <surajsaiyer@gmail.com>
2023-11-09 17:17:48 +0530

    pop up done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Card from "./Components/Card/Card";
 import Button from "./Components/Button/Button";
 import Modal from "./Components/Modal/Modal";
+import Form1 from "./Components/Modal/Form/Form1";
+import Form2 from "./Components/Modal/Form/Form2";
 
 function App() {
   const [jobs, setJobs] = useState([]);
@@ -37,9 +39,15 @@ function App() {
 
   return (
     <>
-      <Modal isModalOpen={showModal} isModalClose={closeModal} />
+      {/* <Modal /> */}
+      <Form1 isModalOpen={showModal} isModalClose={closeModal} />
+      <Form2 />
       <Button onClick={openModal} label="Apply Now" />
-      <div className="flex flex-wrap bg-[#D8D8D8] gap-12 justify-center align-center py-4">
+      <div
+        className={` ${
+          showModal ? "flex opacity-20 fixed overflow-hidden " : ""
+        } flex-wrap bg-[#D8D8D8] flex gap-12 justify-center align-center py-4`}
+      >
         {jobs.map((job, index) => {
           return index < 10 && <Card key={job.id} data={job} />;
         })}
--- src/Components/Modal/Form/Form1.jsx
+++ src/Components/Modal/Form/Form1.jsx
@@ -10,43 +10,59 @@ const Form1 = ({ isModalOpen, isModalClose }) => {
   }
 
   return (
-    <div className="flex justify-center align-center py-10 h-[1024px] bg-[#F2F5F7] ">
+    <div className="flex justify-center align-center py-10 h-[1024px]  fixed top-0 right-0 z-10  w-full">
       <form
         className="flex  w-[560px] bg-white h-[500px] flex-col p-[32px] rounded"
         action="submit_job_application.php"
         method="post"
       >
-        <div className="flex flex-col gap-6">        
-        <div className="flex justify-between">
-          <h1>Create A Job </h1>
-          <span>Step 1</span>
-        </div>
-        <div className="flex flex-col justify-start ">
-          <Label text="Job Title" />
-          <Input type="text" className="w-[489px]" placeholder='ex. UX UI Designer' />
-        </div>
-        <div className="flex flex-col justify-start ">
-          <Label text="Company Name" />
-          <Input type="text" className="w-[489px]" placeholder='ex. Google' />
-        </div>
-        <div className="flex flex-col justify-start ">
-          <Label text="Industry" />
-          <Input type="text" className="w-[489px]" placeholder='ex. Information Technology ' />
-        </div>
-        <div className="flex gap-6 justify-between">
-        <div className="flex flex-col justify-start ">
-          <Label text="Location" />
-          <Input type="text" className='w-full' placeholder='ex. Chennai ' />
-        </div>        
-        <div className="flex flex-col justify-start ">
-          <Label text="Remote Type" />
-          <Input type="text" className='w-full' placeholder='ex. In-office ' />
-        </div>
-        </div>
-        <div className="flex justify-evenly">
-          <Button onClick={isModalClose} label="Close" />
-          <Button type="submit" label="Submit" />
-        </div>
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-between">
+            <h1>Create A Job </h1>
+            <span>Step 1</span>
+          </div>
+          <div className="flex flex-col justify-start ">
+            <Label text="Job Title" />
+            <Input
+              type="text"
+              className="w-[489px]"
+              placeholder="ex. UX UI Designer"
+            />
+          </div>
+          <div className="flex flex-col justify-start ">
+            <Label text="Company Name" />
+            <Input type="text" className="w-[489px]" placeholder="ex. Google" />
+          </div>
+          <div className="flex flex-col justify-start ">
+            <Label text="Industry" />
+            <Input
+              type="text"
+              className="w-[489px]"
+              placeholder="ex. Information Technology "
+            />
+          </div>
+          <div className="flex gap-6 justify-between">
+            <div className="flex flex-col justify-start ">
+              <Label text="Location" />
+              <Input
+                type="text"
+                className="w-full"
+                placeholder="ex. Chennai "
+              />
+            </div>
+            <div className="flex flex-col justify-start ">
+              <Label text="Remote Type" />
+              <Input
+                type="text"
+                className="w-full"
+                placeholder="ex. In-office "
+              />
+            </div>
+          </div>
+          <div className="flex justify-evenly">
+            <Button onClick={isModalClose} label="Close" />
+            <Button type="submit" label="Next" />
+          </div>
         </div>
       </form>
     </div>
